679dccf016e41f52bf50100f43e9b908345a8f4a
DiegoRamTo <diego_r@evolucionismo.mx>
2025-04-06 22:28:13 -0600

    Post actualizado

--- database/migrations/2024_11_01_203145_create_incidentes_table.php
+++ database/migrations/2024_11_01_203145_create_incidentes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('incidente', function (Blueprint $table) {
+        Schema::create('incidentes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_autobus');
             $table->unsignedBigInteger('id_corrida')->nullable();
